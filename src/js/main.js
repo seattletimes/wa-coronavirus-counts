da848b94c6b040a832c48ed24ef087c3b80877e3
llflannery <lflannery@seattletimes.com>
2020-11-27 21:38:32 -0800

    note added to new cases page

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ const d3 = require("d3");
 
 
 ////CHANGE ME WHEN DAY CHANGES - FOR DAY OF DATA/////
-var day_var = "1124";
+var day_var = "1125";
 
 //// change me every month ////
 var monthTicks = ["3/1", "4/1", "5/1", "6/1", "7/1","8/1","9/1","10/1","11/1"];
